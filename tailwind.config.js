a5c2a2089845ba7e3339535f8d30170607905990
Silvia Martínez Báez <smartinezbae@uoc.edu>
2025-01-16 21:37:23 +0100

    refactor: remove custom base clases and variables; extend tailwind colors theme;

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#1b98e0',
+        yellow: '#f4c61f',
+        'blue-dark': 'color(display-p3 0 .478 1)',
+        white: '#fff',
+        'white-grey': 'color(display-p3 .974 .98 .98)',
+        'white-transparent': '#ffffff8c',
+        'white-transparent-2': '#ffffff1a',
+        grey: 'color(display-p3 .424 .463 .494)',
+        green: 'color(display-p3 .7569 1 .5294)',
+      }
+    },
   },
   plugins: [],
 }
